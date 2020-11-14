9ea706efaf789bf595f714748a0417f545d675ce
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-14 23:28:01 +0100

    Some cleaning

--- src/main/java/com/mediscreen/patient/repository/PatientRepository.java
+++ src/main/java/com/mediscreen/patient/repository/PatientRepository.java
@@ -4,8 +4,6 @@ import com.mediscreen.patient.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 /**
  * Interface extending the JpaRepository interface to manage CRUD methods for Patient entities, using Spring DataJPA.
  */
