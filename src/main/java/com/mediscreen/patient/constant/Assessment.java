5548076297e85eca53b1cb91e09759c42053278c
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-30 00:00:42 +0100

    Work on Constant layer

---
+++ src/main/java/com/mediscreen/patient/constant/Assessment.java
@@ -0,0 +1,11 @@
+package com.mediscreen.patient.constant;
+
+/**
+ * Enumeration materializing the possible results of diabetes risk.
+ */
+public enum Assessment {
+    None,
+    Borderline,
+    InDanger,
+    EarlyOnset
+}
