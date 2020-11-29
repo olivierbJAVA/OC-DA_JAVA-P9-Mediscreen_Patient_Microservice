15ddb4322ec4bcf2193cc13d80ecb16d0fe09866
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-30 00:06:13 +0100

    Work on PatientApplication.java

--- src/main/java/com/mediscreen/patient/PatientApplication.java
+++ src/main/java/com/mediscreen/patient/PatientApplication.java
@@ -2,11 +2,13 @@ package com.mediscreen.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Class in charge of launching the Patient Microservices in charge of managing patient data.
  */
 @SpringBootApplication
+@EnableFeignClients("com.mediscreen.patient")
 public class PatientApplication {
 
 	public static void main(String[] args) {
